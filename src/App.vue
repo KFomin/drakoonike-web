307c096670bfaa129e9ec72035cf4cdda011dbb9
KFomin <fominkonstantiin@gmail.com>
2025-03-24 19:19:15 +0200

    extract loader styles to main

--- src/App.vue
+++ src/App.vue
@@ -100,24 +100,4 @@ onBeforeMount(async () => {
   align-items: center;
   width: 100%;
 }
-
-.loader {
-  width: 32px;
-  height: 32px;
-  border: 5px solid #FFF;
-  border-bottom-color: transparent;
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  animation: rotation 1s linear infinite;
-}
-
-@keyframes rotation {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
 </style>
--- src/components/Quest.vue
+++ src/components/Quest.vue
@@ -130,25 +130,6 @@ const questClass = () => {
   height: 48px;
 }
 
-.loader {
-  width: 32px;
-  height: 32px;
-  border: 5px solid #FFF;
-  border-bottom-color: transparent;
-  border-radius: 50%;
-  display: inline-block;
-  box-sizing: border-box;
-  animation: rotation 1s linear infinite;
-}
-
-@keyframes rotation {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
 
 .quest__description {
   max-width: 80%;
