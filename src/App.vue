aa0d7bb6dbb133bf337359a191ff77bf2b4413ed
KFomin <fominkonstantiin@gmail.com>
2025-03-24 16:22:36 +0200

    refactor App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,49 @@
 <script setup>
-import {onBeforeMount, onMounted, ref} from 'vue';
+import {onBeforeMount, reactive} from 'vue';
 import StartTheGame from './components/StartTheGame.vue';
-import {GET_QUEST_BOARD, POST_START_GAME,} from '@/resource/api.js';
+import {GET_QUEST_BOARD, POST_START_GAME} from '@/resource/api.js';
 import {GameStartResponse, QuestModel} from '@/models/models.js';
 import Game from "@/components/Game.vue";
-import {getLocalStorageGameData} from "@/service/utils.js"
+import {getLocalStorageGameData} from "@/service/utils.js";
 
-const gameData = ref(null);
-const gameEndData = ref(null);
-const loading = ref(true);
+const state = reactive({
+  gameData: null,
+  gameEndData: null,
+  loading: true,
+});
 
-const startGame = async () => {
+const onGameStartClicked = async () => {
   try {
-    const gameDataResponse = await POST_START_GAME();
-    gameData.value = new GameStartResponse(
-        gameDataResponse.gameId,
-        gameDataResponse.lives,
-        gameDataResponse.gold,
-        gameDataResponse.score,
-        gameDataResponse.level,
-        gameDataResponse.turn,
-    )
-    gameEndData.value = null;
+    POST_START_GAME().then((gameDataResponse) => {
+      state.gameData = new GameStartResponse(
+          gameDataResponse.gameId,
+          gameDataResponse.lives,
+          gameDataResponse.gold,
+          gameDataResponse.score,
+          gameDataResponse.level,
+          gameDataResponse.turn,
+      );
+      state.gameEndData = null;
+    });
   } catch (error) {
-    console.error("Error : ", error);
+    console.error("Error: ", error);
   }
 };
 
 const onGameEnd = (endGameData) => {
-  gameEndData.value = {
+  state.gameEndData = {
     score: endGameData.score,
     level: endGameData.level,
     gold: endGameData.gold,
-    turn: endGameData.turn
+    turn: endGameData.turn,
   };
-}
+};
 
-onBeforeMount(() => {
+onBeforeMount(async () => {
   const storedGameData = getLocalStorageGameData();
   if (storedGameData) {
     try {
-      let localStorageGameData = new GameStartResponse(
+      const localStorageGameData = new GameStartResponse(
           storedGameData.gameId,
           storedGameData.lives,
           storedGameData.gold,
@@ -49,49 +52,49 @@ onBeforeMount(() => {
           storedGameData.turn,
       );
 
+      //check if game session still alive.
       GET_QUEST_BOARD(localStorageGameData.gameId).then((res) => {
-        let quest = new QuestModel(
-            res.adId,
-            res.message,
-            res.reward,
-            res.expiresIn,
-            res.probability,
-            res.encrypted
+        const quest = new QuestModel(
+            res[0].adId,
+            res[0].message,
+            res[0].reward,
+            res[0].expiresIn,
+            res[0].probability,
+            res[0].encrypted
         );
-        if (!quest) {
-          loading.value = false;
-          return;
+
+        //successfully gettings quest means that game session with our gameId exists.
+        if (quest) {
+          state.gameData = localStorageGameData;
         }
-        gameData.value = localStorageGameData;
-        loading.value = false;
       });
+
     } catch (error) {
-      console.log("Failed to set up game from localStorage", error);
+      console.error("Failed to set up game from localStorage", error);
+    } finally {
+      state.loading = false;
     }
   }
-
-  loading.value = false;
+  state.loading = false;
 });
-
 </script>
 
 <template>
   <div class="wrapper">
-    <div v-if="loading" class="loader"></div>
-    <template v-if="!loading">
-      <StartTheGame v-if="!gameData || gameEndData" :start-game="startGame" :game-end-data="gameEndData"/>
-      <Game v-if="gameData && !gameEndData" :game-data="gameData" :on-game-end="onGameEnd"/>
+    <div v-if="state.loading" class="loader"></div>
+    <template v-if="!state.loading">
+      <StartTheGame v-if="!state.gameData || state.gameEndData" :start-game="onGameStartClicked"
+                    :game-end-data="state.gameEndData"/>
+      <Game v-if="state.gameData && !state.gameEndData" :game-data="state.gameData" :on-game-end="onGameEnd"/>
     </template>
   </div>
 </template>
 
 <style scoped>
-
 .wrapper {
   display: flex;
   justify-content: center;
   align-items: center;
-  place-items: center;
   width: 100%;
 }
 
